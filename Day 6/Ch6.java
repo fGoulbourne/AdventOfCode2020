37bcbc239feeefc73e44fa5fb3be3d22629788a3
jGoulbourne <jamesg2000@googlemail.com>
2020-12-06 14:24:24 +0000

    Day 6

---
+++ Day 6/Ch6.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Ch6 {
+
+  public static void main(String[] args) throws Exception {
+    part1();
+    part2();
+  }
+
+  public static void part1() throws Exception {
+    BufferedReader br = new BufferedReader(new FileReader("questions.txt"));
+    HashSet<Character> questions = new HashSet<>();
+    String line;
+    int qSum = 0;
+
+    while (br.ready()) {
+      line = br.readLine();
+      if(line != null && line.length() != 0) {
+        for (int i = 0; i < line.length(); i++) {
+          questions.add(line.charAt(i));
+        }
+      } else {
+        qSum += questions.size();
+        questions = new HashSet<>();
+      }
+
+    }
+    qSum += questions.size();
+    System.out.println(qSum);
+  }
+
+  public static void part2() throws Exception {
+    BufferedReader br = new BufferedReader(new FileReader("questions.txt"));
+    HashSet<Character> questionsOne = new HashSet<>();
+    HashSet<Character> questionsAll = new HashSet<>();
+    String line;
+    char ch;
+    int qSum = 0;
+    for(int i = 0; i < 26; i++) {
+      questionsAll.add((char)('a' + i));
+    }
+
+    while(br.ready()) {
+
+      line = br.readLine();
+      if(line != null && line.length() != 0) {
+        for (int i = 0; i < line.length(); i++) {
+          questionsOne.add(line.charAt(i));
+        }
+        for(int j = 0; j < 26; j++) {
+          ch = (char)('a' + j);
+          if (!questionsOne.contains(ch)) {
+            questionsAll.remove(ch);
+          }
+        }
+        questionsOne = new HashSet<>();
+      } else {
+        qSum += questionsAll.size();
+        for(int i = 0; i < 26; i++) {
+          questionsAll.add((char)('a' + i));
+        }
+      }
+    }
+    qSum += questionsAll.size();
+
+    System.out.println(qSum);
+  }
+}
